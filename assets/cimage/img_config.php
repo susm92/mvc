e79159df2284869650cf6ed04fb02aa282b08458
susm92 <sucrosmrkovic92@gmail.com>
2023-03-30 09:52:39 +0200

    first draft

---
+++ assets/cimage/img_config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "mode"         => "production",
+    "image_path"   =>  __DIR__ . "/../../public/img/",
+    "cache_path"   =>  __DIR__ . "/../../cache/cimage/",
+    "autoloader"   =>  __DIR__ . "/../../vendor/autoload.php",
+];
---
+++ src/Controller/LuckyControllerTwig.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerTwig extends AbstractController
+{
+    #[Route("/lucky", name: "lucky")]
+    public function number(): Response
+    {
+        $random_img = ['random_pic_1', 'random_pic_2', 'random_pic_3', 'random_pic_4'];
+        
+        $number = random_int(100, 10000);
+        $random_pic = $random_img[array_rand($random_img, 1)];
+
+        $data = [
+            'number' => $number,
+            'pic' => $random_pic
+        ];
+
+        return $this->render('lucky.html.twig', $data);
+    }
+}

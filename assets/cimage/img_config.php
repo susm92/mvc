51aee0156a78fa0faf7ca4b033aeeefdfa98b892
susm92 <sucrosmrkovic92@gmail.com>
2023-03-30 19:46:40 +0200

    removed cimage installation and apporiate files

--- assets/cimage/img_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    "mode"         => "production",
-    "image_path"   =>  __DIR__ . "/../../public/img/",
-    "cache_path"   =>  __DIR__ . "/../../cache/cimage/",
-    "autoloader"   =>  __DIR__ . "/../../vendor/autoload.php",
-];

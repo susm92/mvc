baa765a0f01535332f7d7a68bc9afd8398e241e5
susm92 <sucrosmrkovic92@gmail.com>
2023-04-11 10:24:39 +0200

    added functionality for card draw and showValue of card

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Deck\Card;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,5 +14,18 @@ class CardGameController extends AbstractController
     public function home(): Response
     {
         return $this->render('cardGame/home.html.twig');
+    }
+
+    #[Route('game/card/test/draw', name:'test_draw')]
+    public function testDraw(): Response
+    {
+        $card = new Card();
+
+        $data = [
+            "card" => $card->draw(),
+            "showValue" => $card->showValue(),
+        ];
+
+        return $this->render('cardGame/test/draw.html.twig', $data);
     }
 }
--- src/Deck/Card.php
+++ src/Deck/Card.php
@@ -1,1 +1,29 @@
 <?php
+
+namespace App\Deck;
+
+class Card
+{
+    protected $cardValue;
+    protected $value = [
+        'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠',
+        'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+        'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+        'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦'
+    ];
+
+    public function __construct()
+    {
+        $this->cardValue = null;
+    }
+
+    public function draw(): void
+    {
+        $this->cardValue = $this->value[array_rand($this->value, 1)];
+    }
+
+    public function showValue(): string
+    {
+        return $this->cardValue;
+    }
+}

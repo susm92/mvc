3b0136e22f81dbb7c2cd8893fc49f9d3f4ccd447
susm92 <sucrosmrkovic92@gmail.com>
2023-05-15 13:59:43 +0200

    Added all Read properties for database and all books in library

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Library;
+use App\Repository\LibraryRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +18,40 @@ class LibraryController extends AbstractController
         return $this->render('library/index.html.twig', [
             'controller_name' => 'LibraryController',
         ]);
+    }
+
+    #[Route('/library/create', name: 'get_book_create', methods: ['GET'])]
+    public function getBookCreate(): Response
+    {
+        return $this->render('library/sites/createBook.html.twig');
+    }
+
+    #[Route('/library/create', name: 'post_book_create', methods: ['POST'])]
+    public function postBookCreate(
+        ManagerRegistry $doctrine
+    ): Response {
+        $entityManager = $doctrine->getManager();
+
+        $book = new Library();
+        $book->setTitle();
+        $book->setIsbn();
+        $book->setAuthor();
+        $book->setImage();
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return new Response('Saved new book to library');
+    }
+
+    #[Route('/library/show', name: 'library_show')]
+    public function showLibrary(
+        LibraryRepository $libraryRepository
+    ): Response {
+        $data = [
+            'library' => $libraryRepository->findAll(),
+        ];
+
+        return $this->render('library/sites/showBooks.html.twig', $data);
     }
 }
